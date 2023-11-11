3c7af1e387357b9aa431f24429bbd6245d67d1b5
hinata-wakaba <pkr5207+dev@gmail.com>
2023-11-11 15:23:07 +0900

    chore(application): init common

--- sisterhood-application/build.gradle.kts
+++ sisterhood-application/build.gradle.kts
@@ -13,16 +13,30 @@ kotlin {
     sourceSets {
         val androidMain by getting {
             dependencies {
+                api(libs.androidx.activity.compose)
+                api(libs.androidx.appcompat)
+                api(libs.androidx.core.ktx)
+            }
+        }
+
+        val commonMain by getting {
+            dependencies {
                 api(compose.material)
                 api(compose.preview)
                 api(compose.runtime)
                 api(compose.uiTooling)
-                api(libs.androidx.activity.compose)
-                api(libs.androidx.appcompat)
-                api(libs.androidx.core.ktx)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotest.runner.junit5)
             }
         }
     }
 }
 
 androidWithDefault()
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+}
