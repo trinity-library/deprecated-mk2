c9e2a2260a0ca59d03aade0879e79edfee4b2648
hinata-wakaba <pkr5207+dev@gmail.com>
2023-11-11 15:31:54 +0900

    chore(hentai): compose runtime

--- sisterhood-hentai/build.gradle.kts
+++ sisterhood-hentai/build.gradle.kts
@@ -1,6 +1,7 @@
 import sisterhood.androidWithDefault
 
 plugins {
+    alias(libs.plugins.compose)
     alias(libs.plugins.kotlin.plugin.serialization)
     id(libs.plugins.android.library.get().pluginId)
     id(libs.plugins.kotlin.multiplatform.get().pluginId)
@@ -14,6 +15,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                api(compose.runtime)
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.client.core)
