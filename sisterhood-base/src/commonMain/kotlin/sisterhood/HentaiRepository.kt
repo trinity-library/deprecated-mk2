225742eea259a7a72a6208c8556f01eccf780db8
hinata-wakaba <pkr5207+dev@gmail.com>
2023-11-14 23:27:17 +0900

    feat(hentai): init hentai repository

---
+++ sisterhood-base/src/commonMain/kotlin/sisterhood/HentaiRepository.kt
@@ -0,0 +1,4 @@
+package sisterhood
+
+interface HentaiRepository {
+}
---
+++ sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/repository/SqlDelightHentaiRepository.kt
@@ -0,0 +1,7 @@
+package sisterhood.hentai.repository
+
+import app.cash.sqldelight.db.SqlDriver
+import sisterhood.HentaiRepository
+
+class SqlDelightHentaiRepository(sqliteDriver: SqlDriver) : HentaiRepository {
+}
