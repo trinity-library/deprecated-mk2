354bcbff68478c1c9cbb6c23242ced4f820d48c4
hinata-wakaba <pkr5207+dev@gmail.com>
2023-11-23 22:52:34 +0900

    fix(hentai): add http client as a param

--- sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/service/KtorHentaiServiceFactory.kt
+++ sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/service/KtorHentaiServiceFactory.kt
@@ -6,6 +6,6 @@ import io.ktor.client.engine.cio.*
 class KtorHentaiServiceFactory {
     fun create(): KtorHentaiService {
         val httpClient = HttpClient(CIO)
-        return KtorHentaiService()
+        return KtorHentaiService(httpClient)
     }
 }
