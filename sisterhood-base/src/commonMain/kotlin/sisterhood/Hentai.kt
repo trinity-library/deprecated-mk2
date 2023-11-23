ab63c5b5950a39afa9efa94367520e92396668b8
hinata-wakaba <pkr5207+dev@gmail.com>
2023-11-23 21:13:01 +0900

    feat(base): remove fetchedAt attribute

--- sisterhood-base/src/commonMain/kotlin/sisterhood/Hentai.kt
+++ sisterhood-base/src/commonMain/kotlin/sisterhood/Hentai.kt
@@ -1,12 +1,10 @@
 package sisterhood
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
 class Hentai constructor(
     val id: Int,
     val title: String,
     val language: HentaiLanguage,
-    val createdAt: Instant,
-    val fetchedAt: Instant = Clock.System.now(),
+    val createdAt: Instant
 )
