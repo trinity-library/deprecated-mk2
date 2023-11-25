6c8960f423638b57c27fb00e1b621d230bfffa10
hinata-wakaba <pkr5207+dev@gmail.com>
2023-11-25 23:41:36 +0900

    refactor(application): separate grid from page

--- sisterhood-application/src/commonMain/kotlin/sisterhood/application/hentai/HentaiPage.kt
+++ sisterhood-application/src/commonMain/kotlin/sisterhood/application/hentai/HentaiPage.kt
@@ -1,28 +1,11 @@
 package sisterhood.application.hentai
 
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 
 @Composable
 fun HentaiPage() {
     val store = remember { HentaiPageStore() }
-    val lazyGridState = rememberLazyGridState()
 
-    LaunchedEffect(lazyGridState.canScrollForward) {
-        store.onRefresh()
-    }
-
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        state = lazyGridState
-    ) {
-        items(store.state.ids) { id ->
-            HentaiItem(id, store::fetchInfo, store::fetchThumbnail)
-        }
-    }
+    HentaiGrid(store.state.ids, store::fetchInfo, store::fetchThumbnail, store::onRefresh)
 }
