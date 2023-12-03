bed74ea609266f01c50843aa700465b91ad8dc76
hinata-wakaba <pkr5207+dev@gmail.com>
2023-12-03 15:53:13 +0900

    chore(hentai): ktor client mock

--- sisterhood-hentai/build.gradle.kts
+++ sisterhood-hentai/build.gradle.kts
@@ -29,6 +29,7 @@ kotlin {
 
         val commonTest by getting {
             dependencies {
+                api(libs.ktor.client.mock)
                 implementation(libs.kotest.runner.junit5)
             }
         }
