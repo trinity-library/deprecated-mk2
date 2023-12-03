b144aa1a7fb48f37691b92e43f89dcd3ff6e1428
hinata-wakaba <pkr5207+dev@gmail.com>
2023-12-03 18:02:55 +0900

    feat(hentai): handle cache/repo errors

--- sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/cache/SqlDelightHentaiCache.kt
+++ sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/cache/SqlDelightHentaiCache.kt
@@ -8,15 +8,29 @@ import sisterhood.domain.HentaiImage
 class SqlDelightHentaiCache(sqliteDriver: SqlDriver) : HentaiCache {
     private val db = HentaiCache(driver = sqliteDriver)
 
-    override suspend fun readThumbnail(id: HentaiId): HentaiImage? =
+    override suspend fun readThumbnail(id: HentaiId): HentaiImage? = try {
         db.thumbnailCacheQueries.select(id).executeAsOneOrNull()?.thumbnail
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        null
+    }
 
-    override suspend fun readPage(id: HentaiId, pageNumber: Int): HentaiImage? =
+    override suspend fun readPage(id: HentaiId, pageNumber: Int): HentaiImage? = try {
         db.pageCacheQueries.select(id, pageNumber.toLong()).executeAsOneOrNull()?.page
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        null
+    }
 
-    override suspend fun writeThumbnail(id: HentaiId, thumbnail: HentaiImage) =
+    override suspend fun writeThumbnail(id: HentaiId, thumbnail: HentaiImage) = try {
         db.thumbnailCacheQueries.insert(id, thumbnail)
+    } catch (t: Throwable) {
+        t.printStackTrace()
+    }
 
-    override suspend fun writePage(id: HentaiId, pageNumber: Int, page: HentaiImage) =
+    override suspend fun writePage(id: HentaiId, pageNumber: Int, page: HentaiImage) = try {
         db.pageCacheQueries.insert(id, pageNumber.toLong(), page)
+    } catch (t: Throwable) {
+        t.printStackTrace()
+    }
 }
--- sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/repository/SqlDelightHentaiRepository.kt
+++ sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/repository/SqlDelightHentaiRepository.kt
@@ -13,13 +13,20 @@ class SqlDelightHentaiRepository(sqliteDriver: SqlDriver) : HentaiRepository {
         )
     )
 
-    override suspend fun insert(hentai: Hentai) =
+    override suspend fun insert(hentai: Hentai) = try {
         db.hentaiSpecQueries
             .insert(hentai.id, hentai.title, hentai.language, hentai.createdAt.epochNanoseconds)
+    } catch (t: Throwable) {
+        t.printStackTrace()
+    }
 
-    override suspend fun select(vararg ids: HentaiId): List<Hentai> =
+    override suspend fun select(vararg ids: HentaiId): List<Hentai> = try {
         db.hentaiSpecQueries
             .select(ids.toList())
             .executeAsList()
             .map { Hentai(it.id, it.title, it.language, Instant.fromEpochNanoseconds(it.createdAt)) }
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        emptyList()
+    }
 }
