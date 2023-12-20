a3662d4b904f10e0fc9478eed8afaaa13f690e1c
hinata-wakaba <pkr5207+dev@gmail.com>
2023-12-20 23:48:02 +0900

    style(hentai): fix styles

--- sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/infrastructure/JsonColumnAdapter.kt
+++ sisterhood-hentai/src/commonMain/kotlin/sisterhood/hentai/infrastructure/JsonColumnAdapter.kt
@@ -5,11 +5,8 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-inline fun <reified T : Any> jsonColumnAdapter() =
-    object : ColumnAdapter<@Serializable T, String> {
-        override fun decode(databaseValue: String): T =
-            Json.decodeFromString(databaseValue)
+inline fun <reified T : Any> jsonColumnAdapter() = object : ColumnAdapter<@Serializable T, String> {
+    override fun decode(databaseValue: String): T = Json.decodeFromString(databaseValue)
 
-        override fun encode(value: T): String =
-            Json.encodeToString(value)
-    }
+    override fun encode(value: T): String = Json.encodeToString(value)
+}
